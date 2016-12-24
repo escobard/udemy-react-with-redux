95108b58a3430c2b1f25cb09c0c21297b420468d
Daniel Escobar <escobard90@gmail.com>
2016-12-23 23:53:39 -0700

    type: feat
    added section 1 lesson 14 notes and files, search bar component was created and added to index.js

---
+++ proj-1/proj-files-lesson-13-20/src/components/search_bar.js
@@ -0,0 +1,23 @@
+// =============================================================
+// 
+//  search_bar.js
+// 
+// Review notes of proj-1-files-lesson-13-20 in index.js for notes prior to this point
+// 
+// =============================================================
+
+// imports react 
+// quick note, imports must be used for JSX to work on any React components
+import React from 'create';
+
+
+// creates the const for the SearchBar
+const SearchBar = () => {
+ 	return <input />
+};
+
+// this exports the SearchBar const for any other JS file to grab
+export default SearchBar;
+// 
+// more details here:
+// very critical to export the exact correct component from each component
--- proj-1/proj-files-lesson-13-20/src/index.js
+++ proj-1/proj-files-lesson-13-20/src/index.js
@@ -2,21 +2,45 @@
 // 
 // 	Index.js
 // 
-// Review notes of proj-1-files-lesson-13-20 for notes prior to to his point
+// Review notes of proj-1-files-lesson-13-20 for notes prior to this point
 // 
 // =============================================================
  
-// Imports
-
+// imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// imports SearchBar
+// 
+// note: this does not work, since to get a component from a directory that is not your app components, you need to add an actual file 
+// reference by using a relative path to the file we are importing it from
+// to grab the correct file; as such, the following will throw an error : import SearchBar from 'search_bar'
+// 
+// this is how to grab from local JS components:
+// 
+import SearchBar from './components/search_bar.js'
+
 // this is the youtube API key to access video data
+// 
 const API_KEY = 'AIzaSyADgejrRxwevOdD8LPoBTpFjIzOQ0mKlVw';
 
-// 'App' components
+
+// 'App' components original : 
+/*
 const App = () => {
 	return <div>Hi!</div>;
+}
+*/
+
+// 'App' components after section 1 lecture 14
+const App = () => {
+	
+	//returns our app container div
+	return <div>
+		
+		// returns our serch bar component
+		<SearchBar />
+	</div>
 }
 
 // 'App' render into the DOM
