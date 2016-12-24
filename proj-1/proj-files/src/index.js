99329f146f96958a422cb87a5e4be06c43c7c791
Daniel Escobar <escobard90@gmail.com>
2016-12-23 19:11:03 -0700

    type: feat
    added files and notes from section 1 lesson 10

--- proj-1/proj-files/src/index.js
+++ proj-1/proj-files/src/index.js
@@ -38,19 +38,43 @@ import ReactDOM from 'react-dom';
 // create app const. more on const here : https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/const
 // const declares a variable that is never going to change 
 // 
-// 
+// the code here is a type of component that is a class, not an instance 
+// in other words this is a factory that produces components, that get rendered to the DOM
+
 const App = function (){
+	// 
+	// this gets instanced into :
+	// App = function App(){
+	// 	return React.createElement(
+	// 		"div",
+	// 		null,
+	// 		"Hi!"
+	// 	);
+	// };
+	// 
 	// JSX allows us to write what looks like HTML but is really just javascript behind the scenes
 	// JSX can be viewed after transpile from original js here : https://babeljs.io/
 	// this is transpired by babel and webpack that polyfills / compiles code before it hits the browser
+	// 
+	// when you write an HTML element, this is compiled / rendered into : React.createElement('div', null)
+	// This is considered a component 'class' until it gets rended into the DOM, where it becomes a component 'instance'
 	return <div>Hi!</div>;
 };
 
+// the following creates an 'instance' of a 'class', the 'class' being the factory (in this case the const App) the 'instance' being
+// the DOM components that are rendered by React.createElement();
 
-//* take this component's HTML and put it on the page (in the DOM)
+<App></App>
+
+// the above can also be written with <App /> which is valid self closing JSX
+
+/* //* take this component's HTML and put it on the page (in the DOM)
 // this will generate an error, because React is undefined
 // But the function is basically asking React to .render the const App
 //
-*/
+*//
 // ReactDOM is accesed here instead, to actually pass the const App into the ReactDOM.render argument, to pass the const App into the DOM
-ReactDOM.render(App);
+// The problem before was that we were passing a component 'class' into the .render(argument) instead of an 'instance'.
+// this was the code previously :  ReactDOM.render(App);
+// this creates an 'instance' of the const App (or component class App) and passes it to ReactDOM to .render();
+ReactDOM.render(<App />);
