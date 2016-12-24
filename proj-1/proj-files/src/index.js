713077d369dbde39e053bbb24a667f0f824bd646
Daniel Escobar <escobard90@gmail.com>
2016-12-23 18:08:33 -0700

    type: feat
    added JS / JSX files and notes at the end of section 1 lecture 6

--- proj-1/proj-files/src/index.js
+++ proj-1/proj-files/src/index.js
@@ -1,1 +1,17 @@
-// components : something that creates HTML
+// components / views: something that creates HTML
+// create a new component. This component should produce some HTML
+
+
+// es 2016 syntax : REACT and ES 6 code, will be described as such throughout the course
+// create app const. more on const here : https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/const
+// const declares a variable that is never going to change 
+// 
+// 
+const App = function (){
+	// JSX allows us to write what looks like HTML but is really just javascript behind the scenes
+	// this is transpired by bable that polyfills / compiles code before it hits the browser
+	return <div>Hi!</div>;
+};
+
+
+// take this component's HTML and put it on the page (in the DOM)
