d22c810c15e170885c73d9e3be6caf637bc99c8b
Daniel Escobar <escobard90@gmail.com>
2016-12-23 22:37:31 -0700

    type: feat
    added youtube API key and youtube-search API for section 1 lecture 14

---
+++ proj-1/proj-files-lesson-13-20/src/index.js
@@ -0,0 +1,23 @@
+// =============================================================
+// 
+// 	Index.js
+// 
+// Review notes of proj-1-files-lesson-13-20 for notes prior to to his point
+// 
+// =============================================================
+ 
+// Imports
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// this is the youtube API key to access video data
+const API_KEY = 'AIzaSyADgejrRxwevOdD8LPoBTpFjIzOQ0mKlVw';
+
+// 'App' components
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+// 'App' render into the DOM
+ReactDOM.render(<App/>, document.querySelector('.container'))
