f99bf9bc3fc990e1b419d1f23d0723ae4c7e88ab
Daniel Escobar <escobard90@gmail.com>
2016-12-25 18:53:49 -0700

    type: feat
    added section 2 lesson 26 notes

--- proj-1/proj-files-lessons-22-27/src/components/video_list_item.js
+++ proj-1/proj-files-lessons-22-27/src/components/video_list_item.js
@@ -8,7 +8,12 @@
 import React from 'react';
 
 // sets up component
+/* the following is the standard way of setting up the video Constant:
 const VideoListItem = (props) => {
+
+	// creates a const to pull the data from the passed 'video' variable
+	const video = props.video;
+
 
 	return (
 
@@ -16,6 +21,55 @@ const VideoListItem = (props) => {
 
 		);
 
+}; */
+
+// the following is the ES6 way of setting up the video Constant:
+// using {video} as the function argument for VideoListItem pass the variable 'video'
+// into the VideoListItem object, from the props parent object
+const VideoListItem = ({video}) => {
+	
+	// console logs our video so we can inspect it's elements
+	console.log(video);
+
+	// grabs the image's URL from the video array
+	const imageUrl = video.snippet.thumbnails.default.url;
+	
+	// carefully inspect the video object properties, which are attached to the return funtion below, to create the snippet
+	// of each video into our app
+	// 
+	// more info on boostrap column classes = https://getbootstrap.com/examples/grid/
+	// 
+	return (
+
+			<li className="list-group-item">
+				
+				<div className="video-list media">
+					
+					<div className="media-left">
+
+					
+
+						<img className="media-object" src={imageUrl} />
+					
+					</div>
+
+					<div className="media-body">
+
+						<div className="media-heading">
+
+							{video.snippet.title}
+
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</li>
+
+		);
+
 };
+
 
 export default VideoListItem;
