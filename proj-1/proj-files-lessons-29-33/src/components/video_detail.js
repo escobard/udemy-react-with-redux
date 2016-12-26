739e4e734cbdceb396721b2de50e3ec3f81739fd
Daniel Escobar <escobard90@gmail.com>
2016-12-25 21:25:55 -0700

    type: feat
    added notes for section 2 lesson 29

---
+++ proj-1/proj-files-lessons-29-33/src/components/video_detail.js
@@ -0,0 +1,55 @@
+// =============================================================
+// 
+// 	video_detail.js
+//
+// =============================================================
+
+// imports
+import React from 'react';
+
+// videodetail component
+const VideoDetail = ({video}) => {
+
+	// since App is fetching the video files as its already rendering, we need to tell the browser to render
+	// something else until the 'videos' state array has been populated
+	if (!video) {
+		return (
+
+			<div>Loading...</div>
+
+		);
+	}
+
+	// to make sure the iframe is created with unique videos per video detail component, two consts will have to be made
+	const videoId = video.id.videoId;
+
+	// this is the normal JS version to create the video url dynamic variable
+	// const url = 'https://www.youtube.com/embed/' + videoId;
+	// 
+	// this is the ES6 JS version
+	const url = `https://www.youtube.com/embed/${videoId}`;
+
+	return (
+		
+		<div className="video-detail col-md-8">
+			
+			<div className="embed-responsive embed-responsive-16by9">
+				
+				<iframe className="embed-responsive-item" src={url}></iframe>
+
+			</div>
+			<div className="details">
+				
+				<div>{video.snippet.title}</div>
+				<div>{video.snippet.description}</div>
+
+			</div>
+
+		</div>
+
+	);
+
+
+};
+
+export default VideoDetail;
---
+++ proj-1/proj-files-lessons-29-33/src/components/video_list_item.js
@@ -0,0 +1,82 @@
+// =============================================================
+// 
+// 	video_list_item.js
+//
+// =============================================================
+
+// imports 
+import React from 'react';
+
+// sets up component
+/* the following is the standard way of setting up the video Constant:
+const VideoListItem = (props) => {
+
+	// creates a const to pull the data from the passed 'video' variable
+	const video = props.video;
+
+
+	return (
+
+			<li>Video</li>
+
+		);
+
+}; */
+
+// the following is the ES6 way of setting up the video Constant:
+// using {video} as the function argument for VideoListItem pass the variable 'video'
+// into the VideoListItem object, from the props parent object
+// passes another const into the VideoListItem const, which is imported from the App component, then into the list component,
+// and finally to this component
+const VideoListItem = ({video, onVideoSelect}) => {
+	
+	// console logs our video so we can inspect it's elements
+	console.log(video);
+
+	// grabs the image's URL from the video array
+	const imageUrl = video.snippet.thumbnails.default.url;
+	
+	// carefully inspect the video object properties, which are attached to the return funtion below, to create the snippet
+	// of each video into our app
+	// 
+	// more info on boostrap column classes = https://getbootstrap.com/examples/grid/
+	// 
+	// make an event for the list so that whenever a user clicks on that specific video listed item, it runs the onVideoSelect function, 
+	// loading the proper video data for the clicked on item into App.state.selectedVideo state from the main app
+	// 
+	// 
+	// 
+	return (
+
+			<li onClick={() => onVideoSelect(video)} className="list-group-item">
+				
+				<div className="video-list media">
+					
+					<div className="media-left">
+
+
+
+						<img className="media-object" src={imageUrl} />
+					
+					</div>
+
+					<div className="media-body">
+
+						<div className="media-heading">
+
+							{video.snippet.title}
+
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</li>
+
+		);
+
+};
+
+
+export default VideoListItem;
