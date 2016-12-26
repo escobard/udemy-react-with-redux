084af4371198a3d9aee97ae1534f84e0a452cfd5
Daniel Escobar <escobard90@gmail.com>
2016-12-25 18:32:15 -0700

    type: feat
    added notes from section 2 lesson 25

--- proj-1/proj-files-lessons-22-27/src/components/video_list.js
+++ proj-1/proj-files-lessons-22-27/src/components/video_list.js
@@ -21,9 +21,13 @@ const VideoList = (props) => {
 		// assigns the video variable to each index of the videos array then returns a function
 		// also passes the property name video, aka the data of the corresponding index of the array
 		// into the VideoListItem component
+		// 
+		// the key callback allows for a unique key to be assigned to each of the returned elements
+		// to assign the video.etag (an ID of sorts contained within each of the videos array objects)
+		// as an identifier to EACH video list item
 		(video) => {
 
-				return <VideoListItem video={video}/>
+				return <VideoListItem key={video.etag} video={video}/>
 	});
 
 	// to set up the classes for HTML elements we use className in JSX
@@ -39,6 +43,7 @@ const VideoList = (props) => {
 	// 
 	// passes the videoItems const into the VideoList component
 	// 
+	// When adding variables from a const constructor function 
 	return (
 		
 		<ul className="col-md-4 list-group">
