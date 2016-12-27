254132d9a479835d4dfef36e725703192b0ce845
Daniel Escobar <escobard90@gmail.com>
2016-12-26 18:07:31 -0700

    type: feat
    notes and files from section 4 lesson 40

---
+++ proj-2/section-4-39-46-notes/src/reducers/index.js
@@ -0,0 +1,26 @@
+// =============================================================
+// 
+// 	reducers/index.js
+//
+// =============================================================
+// 
+// more on reducers here : http://redux.js.org/docs/basics/Reducers.html
+// in short, a reducer is a function that returns a piece of the application's state / model / data
+// reducers produce the VALUE of our state
+// good to keep in mind that each piece of the app should be named after the folder its in, to be concise
+
+// imports combineReducers component from 'redux'
+import { combineReducers } from 'redux';
+
+// imports the data from reducer_books.js
+import booksReducer from './reducer_books'
+
+// this function below is what maps our state reducers, joining all of them together for react to use
+const rootReducer = combineReducers({
+
+	// this assigns the book key to our bookReducer function
+	books: booksReducer;
+
+});
+
+export default rootReducer;
