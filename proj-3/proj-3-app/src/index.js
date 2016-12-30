65f0772bef8384488c8da4fd723d2dee4b0fc356
Daniel Escobar <escobard90@gmail.com>
2016-12-29 19:25:09 -0700

    type: feat
    section 5 lesson 55-56 notes and files

--- proj-3/proj-3-app/src/index.js
+++ proj-3/proj-3-app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+// adds the MIDDLEWARE to use promises with redux, to handle AJAX requests
+import ReduxPromise from 'redux-promise';
+
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// the ReduxPromise function is passed into the applyMiddleware argument, adding it to our application
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
