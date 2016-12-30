7bf7b24da2dc78d607a531deae88dfd94ade4609
Daniel Escobar <escobard90@gmail.com>
2016-12-29 17:46:12 -0700

    type: feat
    added section 5 lesson 53 notes

---
+++ proj-3/section-5-notes/src/containers/search_bar.js
@@ -0,0 +1,34 @@
+// =============================================================
+// 
+// 	search_bar.js
+//
+// =============================================================
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+	render(){
+
+		return (
+		
+			<form className="input-group">
+
+				<input/>
+				
+				<span className="input-group-btn">
+					
+					<button type="submit" className="btn btn-secondary">Submit</button>
+
+				</span>
+
+			</form>
+
+		);
+
+	}
+
+};
+
+
+export default SearchBar;
