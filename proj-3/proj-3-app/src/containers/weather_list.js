c6d9cd06a0957c6687b30cf80f1ae1547a9e57d8
Daniel Escobar <escobard90@gmail.com>
2016-12-31 16:34:25 -0700

    type: feat
    section 5 lesson 61-62

---
+++ proj-3/proj-3-app/src/containers/weather_list.js
@@ -0,0 +1,57 @@
+// =============================================================
+// 
+// 	actions/index.js
+//
+// =============================================================
+
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+
+	render(){
+		return(
+
+			<table>
+
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+				
+				
+
+				</tbody>
+			</table>
+
+		);
+	}
+
+}
+
+// this is how it's usually written, to pass the state.weather object into this container
+/* function mapStateToProps(state){
+	return {weather: state.weather};
+} */
+
+// we can also write it with EMC6 syntax
+ function mapStateToProps({ weather }){
+
+ 	// this is how the function now looks
+ 	/*
+	return { weather: weather }; */
+
+	//this can be further condensed with ES6 like so:
+	// because both the key and the value object have the same identifier
+	return { weather };
+}
+
+// if we are adding a reducer, we use mapstate to props, which is the first argument of connect, the second argument can be left empty
+// since there are no actions here
+export default connect(mapStateToProps)(WeatherList);
