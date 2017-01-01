97b8d740c49a8fdfc505a5cfa74816f21718e973
Daniel Escobar <escobard90@gmail.com>
2016-12-31 17:04:54 -0700

    type: feat
    section 5 chapter 63 notes and files

--- proj-3/section-5-notes/src/containers/weather_list.js
+++ proj-3/section-5-notes/src/containers/weather_list.js
@@ -7,19 +7,35 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+// imports the chart API for reat, Sparklines, more on that here : https://github.com/borisyankov/react-sparklines
+import { Sparklines, SparklinesLine} from 'react-sparklines';
+
 class WeatherList extends Component {
 	renderWeather(cityData){
-
-		const cityName = cityData.city.name;
 		
+		// this assigns a var to our cityData
+		const cityName = cityData.city.name;
+
+		// for our sparklines charts to work, we basically just need an array of numbers, which is what is passed into the temps const
+		// after mapping the correct data
+		// 
+		// this basically grabs the cityData.list object, then maps out every child array with a function
+		const temps = cityData.list.map(weatherInfo => 
+			// this then grabs all the VALUES of the temp array, which contains the city's temperature for the next 5 days
+			weatherInfo.main.temp);
+		console.log(temps);
+
 		return(
 
 			<tr key={cityName}>
 				
+				<td>{cityName}</td>
 				<td>
-					{cityName}
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
 				</td>
-
+				
 			</tr>
 
 		);
