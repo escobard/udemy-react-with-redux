5670d54afba61f33266e9166875b360c7b460cf6
Daniel Escobar <escobard90@gmail.com>
2017-02-28 00:00:03 -0700

    type: feat
    routes revised section 6 lesson 76

---
+++ proj-4/src/components/posts_index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default () => {
+	return <div>Blog post placeholder.</div>;
+
+};
--- proj-4/src/routes.jsx
+++ proj-4/src/routes.jsx
@@ -6,6 +6,10 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 
+import PostsIndex from './components/posts_index';
+
+// imports the posts component
+
 // the following code sets up the route path for an indvidual component
 // in this case the initial route, which is /, shows our App component
 // // if we did something like this :
@@ -14,7 +18,7 @@ import App from './components/app';
 // 
 // the following allows for component exports, by using export default ();
 
-// avoid creating any components within the routes file, for code cleneliness
+/* avoid creating any components within the routes file, for code cleneliness
 const Greeting = () => {
 	return (
 		<div>HELLO THERE!</div>
@@ -24,15 +28,21 @@ const Greeting = () => {
 /* this is the way to render a single route
 export default (
 <Route path='/' component={App} />
-); */
+); 
 
 // here is how you add more than a single route
 // since the three children route are nested within the main route
 // the other three will automatically have the / added to the url, since the / is the parent route
+// IndexRoute is used to display a component WITH the parent, and ONLY if the parent route is
 export default (
 <Route path="/" component={App}>
+	<IndexRoute component={PostsIndex} />
 	<Route path="greet" component={Greeting}/>
 	<Route path="greet2" component={Greeting}/>
 	<Route path="greet3" component={Greeting}/>
 </Route>
-);
+); */
+export default (
+<Route path="/" component={App}>
+	<IndexRoute component={PostsIndex} />
+</Route>
