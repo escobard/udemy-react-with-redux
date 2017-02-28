abd1b3b6e747e809dab8b98c356468de11d52bb9
Daniel Escobar <escobard90@gmail.com>
2017-02-27 23:41:21 -0700

    type: feat
    section 6 lesson 75, last commit was 74

--- proj-4/src/components/app.js
+++ proj-4/src/components/app.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+  	// using the {this.props.children}, we can render the other components that are nested within app, refer to ./routes.jsx
     return (
-      <div>React simple starter</div>
+      <div>React simple starter
+		{this.props.children}
+      </div>
     );
   }
 }
--- proj-4/src/routes.jsx
+++ proj-4/src/routes.jsx
@@ -13,6 +13,26 @@ import App from './components/app';
 // the comp1 path renders the Component1 component
 // 
 // the following allows for component exports, by using export default ();
+
+// avoid creating any components within the routes file, for code cleneliness
+const Greeting = () => {
+	return (
+		<div>HELLO THERE!</div>
+	);
+}
+
+/* this is the way to render a single route
 export default (
 <Route path='/' component={App} />
+); */
+
+// here is how you add more than a single route
+// since the three children route are nested within the main route
+// the other three will automatically have the / added to the url, since the / is the parent route
+export default (
+<Route path="/" component={App}>
+	<Route path="greet" component={Greeting}/>
+	<Route path="greet2" component={Greeting}/>
+	<Route path="greet3" component={Greeting}/>
+</Route>
 );
