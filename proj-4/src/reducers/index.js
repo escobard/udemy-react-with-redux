a7b8de9bf5a20da642af187265451fbd6e052ec5
Daniel Escobar <escobard90@gmail.com>
2017-02-28 03:21:27 -0700

    type: feat
    section 6 lesson 85 completed

--- proj-4/src/reducers/index.js
+++ proj-4/src/reducers/index.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
 
+// this imports a reducer and sets is as a form reducer
+// first argument is the reducer from redux forms, second reducer is the variable we want the form reducer to be set to
+import { reducer as formReducer } from 'redux-form';
+
+
 const rootReducer = combineReducers({
-	posts: PostsReducer
+	posts: PostsReducer,
+
+	// this needs to be called 'form', or it will NOT work
+	form: formReducer
 });
+
+
 
 export default rootReducer;
