4b8cf1c1ac17dd2693cac50f2ffd49c7e6c4670d
Daniel Escobar <escobard90@gmail.com>
2017-06-29 21:08:59 -0600

    feat: added lifecycle to handle posts_index

--- proj-4/src/components/posts_index.js
+++ proj-4/src/components/posts_index.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions';
 
 export default class PostsIndex extends Component {
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
 	render(){
 		return(
 			<div>
@@ -9,3 +14,6 @@ export default class PostsIndex extends Component {
 		);
 	}
 }
+
+
+export default connect(null, {fetchPosts})(PostsIndex);
